2939ac27df44e45937558b080644295c5a63b684
ProudElm <job_sande@yahoo.co.uk>
2022-08-03 10:14:45 +0200

    Add files via upload
    inital upload version 1.0

---
+++ config_flow.py
@@ -0,0 +1,140 @@
+"""Config flow for SolarEdge Optimizers Data integration."""
+from __future__ import annotations
+
+import logging
+from typing import Any
+
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.core import HomeAssistant
+from homeassistant.data_entry_flow import FlowResult
+from homeassistant.exceptions import HomeAssistantError
+
+from .const import DOMAIN
+from .solaredgeoptimizers import solaredgeoptimizers
+
+_LOGGER = logging.getLogger(__name__)
+
+
+STEP_USER_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required("siteid"): str,
+        vol.Required("username"): str,
+        vol.Required("password"): str,
+    }
+)
+
+
+class SolarEdgeWebAuth:
+    """boe"""
+
+    def __init__(self, siteid: str) -> None:
+        """Initialize."""
+        self.siteid = siteid
+
+    async def authenticate(
+        self, hass: HomeAssistant, username: str, password: str
+    ) -> bool:
+        """Test to check if siteid, username and password are correct."""
+        api = solaredgeoptimizers(
+            siteid=self.siteid, username=username, password=password
+        )
+        # http_result_code = api.check_login()
+        http_result_code = await hass.async_add_executor_job(api.check_login)
+        if http_result_code == 200:
+            # All is well
+            print("All is well")
+            return True
+        else:
+            print("All is NOT well")
+            return False
+
+
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+    """Validate the user input allows us to connect.
+
+    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
+    """
+    hub = SolarEdgeWebAuth(data["siteid"])
+
+    if not await hub.authenticate(hass, data["username"], data["password"]):
+        raise InvalidAuth
+
+    # Return info that you want to store in the config entry.
+    return {"title": "SolarEdge Transformer"}
+
+
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for SolarEdge Optimizers Data."""
+
+    VERSION = 1
+
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Handle the initial step."""
+
+        print("config_flow.py -> async_step_user")
+
+        if user_input is None:
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+            )
+
+        errors = {}
+
+        try:
+            info = await validate_input(self.hass, user_input)
+
+            # api = solaredgeoptimizers(
+            #     siteid=user_input["siteid"],
+            #     username=user_input["username"],
+            #     password=user_input["password"],
+            # )
+
+            # # panelen = api.requestListOfAllPanels()
+            # panelen = await self.hass.async_add_executor_job(api.requestListOfAllPanels)
+            # print(panelen)
+
+        except CannotConnect:
+            errors["base"] = "cannot_connect"
+        except InvalidAuth:
+            errors["base"] = "invalid_auth"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        else:
+            print("Alles is klaar met de config_flow")
+
+            # max = len(panelen)
+            # current = 1
+            # for paneel in panelen:
+            #     print("paneel in config_flow: {}".format(paneel.split("|[0]")))
+            #     print("Bezig met {} van de {}".format(current, max))
+            #     user_input["paneel_info"] = paneel
+            #     if current == max:
+            # print("Run create entry 1")
+            # self.async_create_entry(title=info["title"], data=user_input)
+
+            # print("Run create entry 2")
+            return self.async_create_entry(title=info["title"], data=user_input)
+            #     else:
+            #         self.async_create_entry(
+            #             title="Paneel: {}".format(paneel.split("|[0]")), data=user_input
+            #         )
+            #         current += 1
+
+            # return await self.async_step_dakvlakken()
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+        )
+
+
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(HomeAssistantError):
+    """Error to indicate there is invalid auth."""
